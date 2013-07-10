25634ab27906ea3b9d2958cf720d96e02ad37756
Black <blac2410@hopper.wlu.ca>
2013-07-10 13:04:36 -0400

    Add LifeStats model

--- pongtracker/Statistics/models.py
+++ pongtracker/Statistics/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class LifeStats(models.Model):
+    wins = models.IntegerField(default=0)
+    loses = models.IntegerField(default=0)
+    bounceShots = models.IntegerField(default=0)
+    trickShots = models.IntegerField(default=0)
+    partyFouls = models.IntegerField(default=0)
+    redemptions = models.IntegerField(default=0)
+    cup1Sunk = models.IntegerField(default=0)
+    cup2Sunk = models.IntegerField(default=0)
+    cup3Sunk = models.IntegerField(default=0)
+    cup4Sunk = models.IntegerField(default=0)
+    cup5Sunk = models.IntegerField(default=0)
+    cup6Sunk = models.IntegerField(default=0)
+    
+    def __unicode__(self):
+        return self.LifeStats
+    
+class Ranking(models.Model):
+    mu = models.FloatField(default=25.0)
+    sigma = models.FloatField(default=8.3334)
+    def __unicode__(self):
+        return self.Ranking    
