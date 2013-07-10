d75dc3230d1e666ea27463e806271d9e419599c8
Hengeveld <heng7500@hopper.wlu.ca>
2013-07-10 14:07:11 -0400

    Game models

--- pongtracker/Game/models.py
+++ pongtracker/Game/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from User.models import User
 
-# Create your models here.
+class Team(models.Model):
+    username1 = models.ForgeignKey(User)
+    username2 = models.ForgeignKey(User)
+
+class Game(models.Model):
+    date_played = DateTimeField(auto_now=True)
+    team1 = models.OneToOne(Team)
+    team2 = models.OneToOne(Team)
