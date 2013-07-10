b0285b14791b889a37d195b3bcf2a37fd4f08f41
Hengeveld <heng7500@hopper.wlu.ca>
2013-07-10 15:55:11 -0400

    Game models event and eventtype

--- pongtracker/Game/models.py
+++ pongtracker/Game/models.py
@@ -9,3 +9,16 @@ class Game(models.Model):
     date_played = models.DateTimeField(auto_now=True)
     team1 = models.OneToOneField(Team,related_name="Team1")
     team2 = models.OneToOneField(Team,related_name="Team2")
+
+class Event(models.Model):
+    event_time = models.DateTimeField(auto_now=True)
+    cup1 = models.Boolean(default=False)
+    cup2 = models.Boolean(default=False)
+    cup3 = models.Boolean(default=False)
+    cup4 = models.Boolean(default=False)
+    cup5 = models.Boolean(default=False)
+    cup6 = models.Boolean(default=False)
+    event_type = models.ForeignKey(EventType)
+      
+class EventType(models.Model):
+    typeName = models.CharField(max_length=20)
