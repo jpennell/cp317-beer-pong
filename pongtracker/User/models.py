b8870978d4e6b8b11587f4eda5ca46eaf0211d27
Hengeveld <heng7500@hopper.wlu.ca>
2013-07-10 13:38:42 -0400

    User models

--- pongtracker/User/models.py
+++ pongtracker/User/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    passwd = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=100)
+    height = models.SmallIntegerField()
+    graduation_year = models.DateField()
+    photo = ImageField()
+    institution_id = models.ForeignKey(Institution)
+    is_banned = models.BooleanField()
+    
+    
+class Institution(models.Model):
+    name = models.CharField(max_length=45)
