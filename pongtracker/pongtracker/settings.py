80c72e36050a49fc528d3ab24ad036bb40cc6507
Black <blac2410@hopper.wlu.ca>
2013-07-11 11:08:01 -0400

    Removed erins info from settings

--- pongtracker/pongtracker/settings.py
+++ pongtracker/pongtracker/settings.py
@@ -12,10 +12,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'cram8680',                      # Or path to database file if using sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'cram8680',
-        'PASSWORD': 'bigtop',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'hopper.wlu.ca',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
