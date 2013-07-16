eccf2c7039494032fcd7ff748beb37a4a5e4dc27
Black <blac2410@hopper.wlu.ca>
2013-07-16 18:10:59 -0400

    Got css to work on index

--- pongtracker/pongtracker/local_settings.py
+++ pongtracker/pongtracker/local_settings.py
@@ -1,10 +1,10 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'heng7500', # Or path to database file if using sqlite3.
+        'NAME': 'blac2410', # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'heng7500',
-        'PASSWORD': 'gsx1br48',
+        'USER': 'blac2410',
+        'PASSWORD': 'bigtop3',
         'HOST': 'hopper.wlu.ca', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306', # Set to empty string for default.
     }
