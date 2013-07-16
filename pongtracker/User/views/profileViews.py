a84ba5937fdc6d965844cbbb532677ef7d3715b9
Cramer Erin 070318680 <cram8680@hopper.wlu.ca>
2013-07-15 20:30:03 -0400

    updating admin.py

--- pongtracker/User/views/profileViews.py
+++ pongtracker/User/views/profileViews.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from User.models import Profile, Institution
+from User.models import Institution
 from django.core.context_processors import csrf
 from django.template import RequestContext
 
