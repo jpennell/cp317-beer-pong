e57d1dc6e0ab74146e58f5d8ab1bffe18d3b6421
James Pennell <pennell.james@gmail.com>
2013-07-15 21:03:18 -0400

    Update user admin

--- pongtracker/User/admin.py
+++ pongtracker/User/admin.py
@@ -1,21 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
 from User.models import PongUser
 from User.models import Institution
 
 admin.site.register(Institution)
-## Define an inline admin descriptor for UserProfile model
-## which acts a bit like a singleton
-#class ProfileInline(admin.StackedInline):
-#    model = PongUser
-#    can_delete = False
-#    verbose_name_plural = 'profile'
-#
-## Define a new User admin
-#class UserAdmin(UserAdmin):
-#    inlines = (ProfileInline, )
-#
-## Re-register UserAdmin
-admin.site.register(PongUser)
-#admin.site.register(User, UserAdmin)
+
+class PongUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = PongUser
+
+class PongUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = PongUser
+
+class PongUserAdmin(UserAdmin):
+    form = PongUserChangeForm
+    add_form = PongUserCreationForm
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('is_banned',)}),
+    )
+
+admin.site.register(PongUser, PongUserAdmin)
