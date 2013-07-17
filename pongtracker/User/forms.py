7bf4472da502a9db9e58b1d77661509df8f8a09f
Black <blac2410@hopper.wlu.ca>
2013-07-16 20:05:48 -0400

    renamed editProfileViews

---
+++ pongtracker/User/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class EditProfileForm(forms.Form):
+    year_choices = []
+    for r in range((datetime.datetime.now().year),(datetime.datetime.now().year+10)):
+        year_choices.append((r,r))
+        
+    firstname = forms.CharField(max_length=250)
+    lastname = forms.CharField(max_length=250)
+    email = forms.EmailField(max_length=250)
+    height = forms.SmallIntegerField(null = True, blank = True)
+    photo = forms.CharField(max_length=100, null = True, blank = True)
+    graduation_year = forms.IntegerField(('year'), max_length=4, choices=year_choices, default=datetime.datetime.now().year+1)
+    institution = forms.ModelChoiceField(Institution.object.all())
+    
