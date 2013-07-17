32335900a089c8685fba0719fb96682c51da6ed6
George Lifchits <george.lifchits@gmail.com>
2013-07-16 20:46:30 -0400

    Commited edit profile stuff

--- pongtracker/User/forms.py
+++ pongtracker/User/forms.py
@@ -7,11 +7,13 @@ class EditProfileForm(forms.Form):
     for r in range((datetime.datetime.now().year),(datetime.datetime.now().year+10)):
         year_choices.append((r,r))
         
+    year_tuple = (year_choices)
+        
     firstname = forms.CharField(max_length=250)
     lastname = forms.CharField(max_length=250)
     email = forms.EmailField(max_length=250)
-    height = forms.SmallIntegerField(null = True, blank = True)
-    photo = forms.CharField(max_length=100, null = True, blank = True)
-    graduation_year = forms.IntegerField(('year'), max_length=4, choices=year_choices, default=datetime.datetime.now().year+1)
-    institution = forms.ModelChoiceField(Institution.object.all())
+    height = forms.IntegerField(max)
+    photo = forms.ImageField()
+    #graduation_year = forms.ChoiceField(('year'), max_length=4, choices=year_tuple)
+    #institution = forms.ModelChoiceField(Institution.all())
     
--- pongtracker/User/views/editProfileView.py
+++ pongtracker/User/views/editProfileView.py
@@ -2,38 +2,38 @@ from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 from User.forms import EditProfileForm
+from django.template import Context
 
 
-
-def editProfile( request ):
+def editProfile(request):
     """{{Description}}
 
     Keyword arguments:
-    variable -- description
-    variable -- description
-
+    variable -- description 
+    variable -- description 
+    
     Contributors:
     Quinton Black
-
+    
     Output:
-
+        
     """
     form = EditProfileForm()
-    context = Context({'title': 'Add Item', 'form': form})
+    context = Context({'title': 'Edit Profile', 'form': form})
     
     return render(request,'user/editProfile.html',context)
 
 
 
 
-def _updatePassword( password ):
-
+def _updatePassword(password):
+    
     return
 
-def _updateUser( username, password, firstName, lastName, email, height, yearOfGradution, userProfilePhoto, isBanned ):
-
-
-
+def _updateUser(username,password,firstName,lastName,email,height,yearOfGradution,userProfilePhoto,isBanned):
+    
+    
+    
     return
 
 
