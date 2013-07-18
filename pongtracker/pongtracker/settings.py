e4bc539cf72abe7427d0fe48b9f3bd1dd43a77af
Hengeveld <heng7500@hopper.wlu.ca>
2013-07-18 10:34:26 -0400

    local settings

--- pongtracker/pongtracker/settings.py
+++ pongtracker/pongtracker/settings.py
@@ -13,9 +13,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'blac2410',  # Or path to database file if using sqlite3.
+        'NAME': 'heng7500',  # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'blac2410',
+        'USER': 'heng7500',
         'PASSWORD': 'bigtop3',
         'HOST': 'hopper.wlu.ca',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',  # Set to empty string for default.
