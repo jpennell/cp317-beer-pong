9939a1112533257b0dc805def5ce8a738679a30c
Hengeveld <heng7500@hopper.wlu.ca>
2013-07-22 09:42:03 -0400

    rename verify game

--- pongtracker/Game/urls.py
+++ pongtracker/Game/urls.py
@@ -5,5 +5,6 @@ from Game import views
 urlpatterns = patterns('',
                        url(r'create/$','Game.views.createNewGameRequest'),
                        url(r'(?P<game_id>\d+)/play/$','Game.views.scoreGame'),
+                       url(r'(?P<game_id>\d+)/confirm/$','Game.views.scoreGame'),
                        url(r'(?P<game_id>\d+)/summary/$','Game.views.viewGameSummaryRequest'),
 )
