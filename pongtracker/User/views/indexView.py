6dcac7a1f698571f87bb26513db48f4b11e05e7f
Black <blac2410@hopper.wlu.ca>
2013-07-26 20:23:25 -0400

    removed unneccesary index code

--- pongtracker/User/views/indexView.py
+++ pongtracker/User/views/indexView.py
@@ -17,31 +17,9 @@ def viewHomepage(request):
     Output:
         
     """ 
-
-    
-    
-    
-    username = request.GET.get('username','')
-    email = request.GET.get('email','')   
-    usernameState = request.GET.get('usernameState','')
-    emailState = request.GET.get('emailState','')
-    suggestedUsernames = request.GET.get('suggestedUsernames','')
-    state =request.GET.get('state','')
-    
-    
-    if  'registration' in request.GET:
-        if request.GET['registration']=='invalid':
-            print("invalid")
-            registrationForm = RegistrationForm(request.POST)
-            context = Context({'regTitle': 'Register', 'registrationForm': registrationForm, 'username':username, 'email':email})  
-            return render(request, 'user/index.html',context)
-
     registrationForm = RegistrationForm()
-    
-    
-    
-    
-    context = Context({'regTitle': 'Register', 'registrationForm': registrationForm, 'username':username, 'email':email})  
+
+    context = Context({'regTitle': 'Register', 'registrationForm': registrationForm})  
     return render(request, 'user/index.html',context)
     
     
