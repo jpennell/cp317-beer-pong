64b0d1abdb314311ad6aff88083e1a18ea0176ab
George Lifchits <george.lifchits@gmail.com>
2013-07-31 13:47:22 -0400

    minor updates

--- pongtracker/Game/views/scoreGameView.py
+++ pongtracker/Game/views/scoreGameView.py
@@ -4,7 +4,8 @@ from Utilities.utilities import *
 from User.models import PongUser
 
 def scoreGame( request, game_id ):
-    """Creates and undoes events
+    """
+    Creates and undoes events by handling POST requests from the client end
 
     Keyword arguments:
     gameID -- current game (int)
@@ -32,12 +33,9 @@ def scoreGame( request, game_id ):
     # get the info we need
     if request.method == 'POST':
         print( "I am getting a post" )
-
         eventTypeName = request.POST['eventType']
 
-
         if eventTypeName == 'undo':
-
            success = _undoEvent( game )
 
         else:
@@ -50,7 +48,7 @@ def scoreGame( request, game_id ):
             team = request.POST.get( 'team' )
             eventType = EventType.objects.get( _typeName = eventTypeName )
 
-            if eventType == 'death':
+            if eventTypeString == 'death':
                 pass
 
             if team:
@@ -71,8 +69,6 @@ def scoreGame( request, game_id ):
             event = Event.objects.create( _game = game, _eventType = eventType, _user = user,
                                           _cup1 = cups[0], _cup2 = cups[1], _cup3 = cups[2],
                                           _cup4 = cups[3], _cup5 = cups[4], _cup6 = cups[5] )
-
-
 
     return render( request, 'game/play.html', {'game': game} )
     # not sure what to render/redirect to if it even needs to happen
