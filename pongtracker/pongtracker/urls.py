2a4f36b94e82a7f78d8201a3ed2e16cc402d031d
Cramer Erin 070318680 <cram8680@hopper.wlu.ca>
2013-07-30 22:02:02 -0400

    changed to use django url tag

--- pongtracker/pongtracker/urls.py
+++ pongtracker/pongtracker/urls.py
@@ -20,7 +20,7 @@ urlpatterns = patterns('',
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^game/',include('Game.urls')),
 	url(r'^',include('User.urls')),
-	url(r'^rules/', TemplateView.as_view(template_name="pongtracker/rules.html")),
+	url(r'^rules/', TemplateView.as_view(template_name="pongtracker/rules.html"), name='rules'),
 	url(r'^',include('Statistics.urls')),
 	url(r'^$','User.views.viewHomepage'),
 )
