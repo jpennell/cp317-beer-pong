ea56202a3a1ba7e720ca48a57b363ad16ceeff36
George Lifchits <george.lifchits@gmail.com>
2013-08-01 15:21:51 -0400

    commenting and undo activation

--- pongtracker/assets/js/score.js
+++ pongtracker/assets/js/score.js
@@ -332,7 +332,9 @@ var forfeitTeam = function(winners) {
 	postEvent('win', winners, 1, false, false)
 }
 var deathCup = function(team) {
-	/* invoked when a team gets a death cup */
+	/* 
+	 * invoked when a team gets a death cup 
+	 */
 	console.debug('death cup by team ' + team)
 	blamePlayer(team, function(player) {
 		console.debug('Player', player, 'got the death cup')
@@ -346,7 +348,10 @@ var deathCup = function(team) {
  *
  */
 var rotateCups = function() {
-	/* toggles the cups formation */
+	/* 
+	 * toggles the cups formation
+	 */
+	console.debug('rotating cups')
 	var t1 = '#team1 .cups'
 	var t2 = '#team2 .cups'
 	//var transforms = ['transform', '-webkit-transform', '-moz-transform']
@@ -365,12 +370,12 @@ var documentRefresh = function() {
 	/* function called to refresh the state of the page */
 	console.debug('refresh function')
 	refreshCups()
+	refreshUndo()
 }
 $(document.body).ready(function() {
 	/* function to refresh the document on load */
 	console.debug('document loaded')
 	documentRefresh()
-	refreshUndo()
 })
 $(document).delegate('.cup.active:not(".bcup")', 'click', function() {
 	/*
