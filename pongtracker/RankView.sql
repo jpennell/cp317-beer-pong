5f14417e8d48bd583558fd4deb71dec25856dee5
doug3230 <doug3230@mylaurier.ca>
2013-08-03 13:19:42 -0400

    fixed RankView.sql
    I am a genius and completely know what I'm doing (George did it!)

--- pongtracker/RankView.sql
+++ pongtracker/RankView.sql
@@ -3,8 +3,15 @@ CREATE
     SQL SECURITY DEFINER
 VIEW `Statistics_rankView` AS
     select 
-        `r`.`_user_id` AS `id`,
-        round((`r`.`_mu` - (3 * `r`.`_sigma`)), 5) AS `_skillNumber`
-    from
-        `Statistics_ranking` `r`
-    order by (`r`.`_mu` - (3 * `r`.`_sigma`)) desc
+		`r`.`_user_id` AS `id`,
+		round((`r`.`_mu` - (3 * `r`.`_sigma`)), 5) AS `_skillNumber`,
+		(`s`.`_cup1Sunk` + `s`.`_cup2Sunk` + 
+		 `s`.`_cup3Sunk` + `s`.`_cup4Sunk` + 
+		 `s`.`_cup5Sunk` + `s`.`_cup6Sunk`) AS `cups`
+	from
+		`Statistics_ranking` `r`, 
+		`Statistics_lifestats` `s`
+	where `r`.`id` = `s`.`id`
+	order by 
+		(`r`.`_mu` - (3 * `r`.`_sigma`)) desc,
+		`cups` desc
