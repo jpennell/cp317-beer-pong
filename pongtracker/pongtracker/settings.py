67d66a0f7ff4586ac176e2875cc6c9f5a19fff35
Quinton Black <blac2410@mylaurier.ca>
2013-08-14 16:08:10 -0400

    Added click jacking again

--- pongtracker/pongtracker/settings.py
+++ pongtracker/pongtracker/settings.py
@@ -1,6 +1,6 @@
 # Django settings for pongtracker project.
 import os.path
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = ( 
@@ -136,7 +136,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
  )
 
 ROOT_URLCONF = 'pongtracker.urls'
