adb3d39f62b62f386a5d57dac086189df8560dcb
George Lifchits <george.lifchits@gmail.com>
2013-08-14 16:46:24 -0400

    edited urls for hosting images

--- pongtracker/pongtracker/urls.py
+++ pongtracker/pongtracker/urls.py
@@ -4,12 +4,14 @@ from django.views.generic import TemplateView
 
 from django.conf import settings
 
+from django.conf.urls.static import static
+
 # Uncomment the next two lines to enable the admin:
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns( '',
 # Examples:
 # url(r'^$', 'pongtracker.views.home', name='home'),
 # url(r'^pongtracker/', include('pongtracker.foo.urls')),
@@ -19,15 +21,17 @@ urlpatterns = patterns('',
 
 # Uncomment the next line to enable the admin:
 
-	url(r'^admin/', include(admin.site.urls)),
-	url(r'^game/',include('Game.urls')),
-	url(r'^',include('User.urls')),
-	url(r'^rules', TemplateView.as_view(template_name="pongtracker/rules.html"), name="rules"),
-	url(r'^',include('Statistics.urls')),
-	url(r'^$','User.views.viewHomepageRequest',name="index"),
-)
+	url( r'^admin/', include( admin.site.urls ) ),
+	url( r'^game/', include( 'Game.urls' ) ),
+	url( r'^', include( 'User.urls' ) ),
+	url( r'^rules', TemplateView.as_view( template_name = "pongtracker/rules.html" ), name = "rules" ),
+	url( r'^', include( 'Statistics.urls' ) ),
+	url( r'^$', 'User.views.viewHomepageRequest', name = "index" ),
+ )
 
 if settings.DEBUG:
-	urlpatterns += patterns('django.contrib.staticfiles.views',
-		url(r'^static/(?P<path>.*)$', 'serve'),
-)
+	urlpatterns += patterns( 'django.contrib.staticfiles.views',
+		url( r'^static/(?P<path>.*)$', 'serve' ),
+ 	)
+
+	urlpatterns += static( settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
